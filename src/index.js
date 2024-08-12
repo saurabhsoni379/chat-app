27585e3433f18b6efda4041dd44a0f2c83b2c74a
saurabh soni <saurabh@gkmit.co>
2024-08-12 11:00:08 +0530

    change

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { DlgProvider } from './context/DlgProvider';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <DlgProvider>
+
     <App />
-  </DlgProvider>
+  
 );
 
